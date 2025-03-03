057c1701eab1d1025b93c1cf5c43c8256ae8666b
Silverclaw-cloud <silverclaw287@protonmail.com>
2025-03-03 10:32:28 -0500

    Create sub.cpp

---
+++ sub.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sum;
+    int x, y;
+    cout << "enter a number: ";
+    cin >> x;
+    cout << "enter another number: ";
+    cin >> y;
+    sum = x + y;
+    cout << "your sum is " << sum << ".\n";
+  
+}
